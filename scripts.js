cf76ffe45537549b8af0972bd3f23114ba37e455
austintip <austin.d.tipograph@gmail.com>
2021-05-01 11:04:04 -0700

    first git commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const app = new VTTCue({
+
+    el: '#main',
+
+    data: {
+
+        result: " ",
+        responseAvailable: false,
+        apiKey: '<40e78f03dbmsh73ad89779995765p1cfe84jsn495944b9cff8>'
+
+    },
+
+    methods: {
+
+        fetchAPIData(){
+
+        }
+    }
+})
