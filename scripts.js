c6bbf9f74dea2bfc8619477433f805329491f5ec
austintip <austin.d.tipograph@gmail.com>
2021-05-01 11:49:23 -0700

    getting pokemon data back

--- scripts.js
+++ scripts.js
@@ -1,19 +1,38 @@
-const app = new VTTCue({
+const app = new Vue({
 
     el: '#main',
 
     data: {
 
-        result: " ",
+        result: ' ',
         responseAvailable: false,
-        apiKey: '<40e78f03dbmsh73ad89779995765p1cfe84jsn495944b9cff8>'
+        // apiKey: "40e78f03dbmsh73ad89779995765p1cfe84jsn495944b9cff8"
 
     },
 
     methods: {
 
-        fetchAPIData(){
-
+        fetchAPIData() {
+            this.responseAvailable = false;
+            fetch("https://pokeapi.co/api/v2/pokemon/1", {
+                "method": "GET",
+                // "headers": {
+                // }
+            })
+                .then(response => {
+                    if (response.ok) {
+                        return response.json()
+                    } else {
+                        alert("Server returned " + response.status + " : " + response.statusText);
+                    }
+                })
+                .then(response => {
+                    this.result = response.name;
+                    this.responseAvailable = true;
+                })
+                .catch(err => {
+                    console.log(err);
+                });
         }
     }
 })
